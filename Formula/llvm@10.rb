fe28ac14c52b4147348ab1642b5fcf7057a1c33f
Nathan Whitaker <nathan.whitaker01@gmail.com>
2020-10-30 20:10:33 -0400

    llvm@10: update 10.0.1_2 bottle.

--- Formula/llvm@10.rb
+++ Formula/llvm@10.rb
@@ -82,10 +82,10 @@ class LlvmAT10 < Formula
   end
 
   bottle do
+    root_url "https://dl.bintray.com/nathanwhit/bottles-main"
     cellar :any
-    sha256 "d8ad1f1c539c4c643017882878d60bc3a45b7e4a67e4d697ac071d20926d121c" => :catalina
-    sha256 "7a5600152cc5d8c7043b9c92db2194e757e17cae6f9e0a498468c3921efb9770" => :mojave
-    sha256 "9ce34ed1a0be220267b76e8c81d7b4955cca470957a4f921fe7e380b0a81b67f" => :high_sierra
+    rebuild 1
+    sha256 "55a7907b8748db04df0fa3a40485f99ed58e1fe762c8a741c0b15602ddc26c64" => :catalina
   end
 
   # Clang cannot find system headers if Xcode CLT is not installed
